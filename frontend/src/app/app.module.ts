dd2b09a4f92a6dfcb876460ec23188320a991e65
Justin Chung <jchung201@gmail.com>
2019-02-19 19:56:53 -0500

    (auth) Basic authentication integration with components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import * as Auth0 from "auth0-web";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
@@ -5,10 +6,36 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { ExamsApiService } from "./exams/exams-api.service";
 
+import { ExamFormComponent } from "./exams/exam-form.component";
+import { RouterModule, Routes } from "@angular/router";
+import { ExamsComponent } from "./exams/exams.component";
+import { CallbackComponent } from "./callback.component";
+
+const appRoutes: Routes = [
+  { path: "callback", component: CallbackComponent },
+  { path: "new-exam", component: ExamFormComponent },
+  { path: "", component: ExamsComponent }
+];
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    CallbackComponent,
+    ExamFormComponent,
+    ExamsComponent
+  ],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
   providers: [ExamsApiService],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    Auth0.configure({
+      domain: "pyp-today.auth0.com",
+      audience: "pyp.today",
+      clientID: "lagPB1JmXjfGR4obMZBWPSmzDjanfn2X",
+      redirectUri: "http://localhost:4200/callback",
+      scope: "openid"
+    });
+  }
+}
