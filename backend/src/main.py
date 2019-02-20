45dca9111189a04018e67d798ee33eabcf5fbb19
Justin Chung <jchung201@gmail.com>
2019-02-19 19:23:45 -0500

    (Auth)Secured Flask with Auth0

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,6 +2,7 @@
 
 from flask_cors import CORS
 from flask import Flask, jsonify, request
+from .auth import AuthError, requires_auth
 
 from .entities.entity import Session, engine, Base
 from .entities.exam import Exam, ExamSchema
@@ -30,6 +31,7 @@ def get_exams():
 
 
 @app.route('/exams', methods=['POST'])
+@requires_auth
 def add_exam():
     # mount exam object
     posted_exam = ExamSchema(only=('title', 'description'))\
@@ -46,3 +48,10 @@ def add_exam():
     new_exam = ExamSchema().dump(exam).data
     session.close()
     return jsonify(new_exam), 201
+
+
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
